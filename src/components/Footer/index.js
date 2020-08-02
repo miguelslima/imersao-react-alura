d5f821b94eef63e6aab7f973468bb27561e6a5cb
Miguel <miguelsousalima@hotmail.com>
2020-08-02 10:51:16 -0300

    Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import { GitHub, LinkedIn } from '@material-ui/icons'
 
 import logo from '../../assets/gnomoflix.png';
 
@@ -14,6 +15,10 @@ function Footer() {
         Orgulhosamente criado durante a{' '}
         <a href="https://www.alura.com.br/">Imersão React da Alura</a>
       </p>
+
+        <a href="https://github.com/miguelslima" target="_blank"><GitHub style={{ fontSize: 40, paddingRight: 10 }}/></a>
+        <a href="https://www.linkedin.com/in/miguelslima1986/" target="_blank"><LinkedIn style={{ fontSize: 40 }}/></a>
+
     </FooterBase>
   );
 }
